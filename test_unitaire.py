b6106198e81bb16e2210bee6b77182540752884b
arnaud-dg <ad_learning@yahoo.com>
2023-02-14 18:21:19 +0100

    update

--- test_unitaire.py
+++ test_unitaire.py
@@ -1,23 +1,10 @@
 # Fonctiond e test unitaire servant à vérifier le bon déploiement de l'API
 
 # Import des librairies
-pip install pytest
-import pytest
-from app import app as flask_app
+from app import get_data
 import json
 
-# Ajout du décorateur - fonction app()
-@pytest.fixture
-def app():
-    yield flask_app
-
-# Ajout du décorateur - fonction client()
-@pytest.fixture
-def client(app):
-    return app.test_client()
-
-def test_index(app, client):
-    res = client.get('/')
-    assert res.status_code == 200
+# Fonction de test
+def test_get_data():
     expected = {'hello': 'world'}
-    assert expected == json.loads(res.get_data(as_text=True))
+    assert expected == json.loads(get_data())
