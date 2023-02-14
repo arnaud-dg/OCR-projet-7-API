a2b8e0ac19f2f5515ca79b809add78020f9c27c5
arnaud-dg <ad_learning@yahoo.com>
2023-02-14 19:11:02 +0100

    Update test_unitaire.py

--- test_unitaire.py
+++ test_unitaire.py
@@ -2,12 +2,12 @@
 
 # Import des librairies
 from app import get_data
-from app import client_data(
+from app import client_data
 import json
 
 # Fonction de test
 def test_client_data():
-    requete_json = app.client_data("222231")
+    requete_json = app.client_data().get("222231")
     # json.loads("https://api-flask-ocr-projet-7.herokuapp.com/data/client/222231")
     print(requete_json)
     print(requete_json["BURO_CREDIT_ACTIVE_cat_Active_MEAN"])
