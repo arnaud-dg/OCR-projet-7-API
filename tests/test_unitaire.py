a0ff1a19231cfc4dd07aec4f1c4c4623495e3f93
arnaud-dg <ad_learning@yahoo.com>
2023-02-14 23:31:45 +0100

    Update test_unitaire.py

--- tests/test_unitaire.py
+++ tests/test_unitaire.py
@@ -3,6 +3,9 @@
 # Import des librairies
 import pytest
 from app import app
+import flask
+from flask import jsonify
+from flask import request
 # from app import get_data
 # from app import client_data
 import json
@@ -15,8 +18,8 @@ def test_get_data():
     Appel de la fonction client_data (numero "222231") ET
     Vérification du contenu du fichier JSON
     """
-    response = app.test_client().get('/')
-    # json.loads("https://api-flask-ocr-projet-7.herokuapp.com/data/client/222231")
+    response = app.get('/data')
+    # URL = "https://api-flask-ocr-projet-7.herokuapp.com/data/client/222231"
     print(response)
     print(response["BURO_CREDIT_ACTIVE_cat_Active_MEAN"])
     expected = [1.0, -0.061780218046809325]
