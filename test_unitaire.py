625ca5112624778de6c3f103653be9877c0d34f8
arnaud-dg <ad_learning@yahoo.com>
2023-02-15 09:04:14 +0100

    Update test_unitaire.py

--- test_unitaire.py
+++ test_unitaire.py
@@ -1,16 +1,16 @@
+# ------------------------------------------------------------------------
 # Fonction de test unitaire servant à vérifier le bon déploiement de l'API
+# ------------------------------------------------------------------------
 
 # Import des librairies
 import pytest
+from main import add_numbers
+from main import app
 # import flask
 # from flask import jsonify
 # from flask import request
-# # from app import app as flask_app
-# # from app import get_data
-# # from app import client_data
 # import json
-# import numpy as np
-# import pandas as pd
+
 
 # Fonction de test
 # @pytest.fixture
@@ -21,13 +21,6 @@ import pytest
 # def client(app):
 #     return app.test_client()
 
-# def test_get_data(app, client):
-#     res = client.get('/data')
-#     assert res.status_code == 200
-
-from main import add_numbers
-from main import app
-
 def test_main():
     assert sum_numbers([1, 2, 3]) == 6
 
@@ -37,5 +30,4 @@ def test_home():
     """
     # URL = "https://api-flask-ocr-projet-7.herokuapp.com/"
     response = app.test_client().get('/')
-    expected = "<h1>"
-    assert expected in response
+    assert response.status_code == 200
